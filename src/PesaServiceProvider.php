5018df8990c249316a805ff21e3987e5fae7e881
alphaolomi <alphaolomi@users.noreply.github.com>
2022-03-03 14:52:56 +0000

    Fix styling

--- src/PesaServiceProvider.php
+++ src/PesaServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Config;
 use Openpesa\SDK\Pesa;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Throwable;
 
 class PesaServiceProvider extends PackageServiceProvider
 {
